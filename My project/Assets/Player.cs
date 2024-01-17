8169f552646c61441a3d71cd6db8abba3583e6ae
RCooper03 <156470817+RCooper03@users.noreply.github.com>
2024-01-17 13:49:23 -0700

    hello Workflow

---
+++ My project/Assets/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * 0.5f;
+        }
+
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            transform.position += Vector3.back * 0.5f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * 0.5f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * 0.5f;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Vector3.up * 0.5f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.position += Vector3.down * 0.5f;
+        }
+    }
+}
